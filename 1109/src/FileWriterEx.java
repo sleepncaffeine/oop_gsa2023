53aaadd52777e9185936fbd216c4b2cf29312063
Jinjae Park <parkjinjae0627@gmail.com>
2023-11-09 11:44:00 +0900

    update

---
+++ 1109/src/FileWriterEx.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class FileWriterEx {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        FileWriter fout = null;
+        try {
+            fout = new FileWriter("test.txt");
+            while (true) {
+                String line = scanner.nextLine();
+                if (line.length() == 0) {
+                    break;
+                }
+                fout.write(line, 0, line.length());
+                fout.write("\r\n", 0, 2);
+            }
+            fout.close();
+        } catch (IOException e) {
+            System.out.println("입출력 오류");
+        }
+        scanner.close();
+    }
+}
